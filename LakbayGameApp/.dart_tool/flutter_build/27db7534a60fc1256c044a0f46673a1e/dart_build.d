bc5fa7af9cdf744e9c6a2e073b7ee5dd1fbfe4d9
viper357621 <87564641+viper357621@users.noreply.github.com>
2026-05-31 20:45:57 +0800

    adjustments

---
+++ LakbayGameApp/.dart_tool/flutter_build/27db7534a60fc1256c044a0f46673a1e/dart_build.d
@@ -0,0 +1,1 @@
+ C:\\Users\\malri\\.config\\herd\\config\\valet\\Sites\\LakbayGame\\LakbayGameApp\\.dart_tool\\flutter_build\\27db7534a60fc1256c044a0f46673a1e\\dart_build_result.json: 
---
+++ LakbayGameApp/build/app/intermediates/cxx/debug/hos2d6f1/logs/arm64-v8a/configure_command.bat
@@ -0,0 +1,20 @@
+@echo off
+"C:\\Users\\malri\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HC:\\Users\\malri\\flutter\\packages\\flutter_tools\\gradle\\src\\main\\scripts" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=24" ^
+  "-DANDROID_PLATFORM=android-24" ^
+  "-DANDROID_ABI=arm64-v8a" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=arm64-v8a" ^
+  "-DANDROID_NDK=C:\\Users\\malri\\AppData\\Local\\Android\\sdk\\ndk\\28.2.13676358" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\malri\\AppData\\Local\\Android\\sdk\\ndk\\28.2.13676358" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\malri\\AppData\\Local\\Android\\sdk\\ndk\\28.2.13676358\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\malri\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\malri\\.config\\herd\\config\\valet\\Sites\\LakbayGame\\LakbayGameApp\\build\\app\\intermediates\\cxx\\debug\\hos2d6f1\\obj\\arm64-v8a" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\malri\\.config\\herd\\config\\valet\\Sites\\LakbayGame\\LakbayGameApp\\build\\app\\intermediates\\cxx\\debug\\hos2d6f1\\obj\\arm64-v8a" ^
+  "-BC:\\Users\\malri\\.config\\herd\\config\\valet\\Sites\\LakbayGame\\LakbayGameApp\\build\\.cxx\\debug\\hos2d6f1\\arm64-v8a" ^
+  -GNinja ^
+  -Wno-dev ^
+  --no-warn-unused-cli ^
+  "-DCMAKE_BUILD_TYPE=debug"
