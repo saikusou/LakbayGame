26593a5e085b865ff46bcabeb8f8eca3267dbe8c
viper357621 <87564641+viper357621@users.noreply.github.com>
2026-06-06 14:57:56 +0800

    ..

--- LakbayGameApp/lib/Views/game/lesson-one/day-one/act3.dart
+++ LakbayGameApp/lib/Views/game/lesson-one/day-one/act3.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lakbay_game/Views/lesson1.dart';
 
 class LessonOneDayOneActThree extends StatefulWidget {
   const LessonOneDayOneActThree({super.key});
@@ -35,19 +36,23 @@ class _LessonOneDayOneActThreeState extends State<LessonOneDayOneActThree> {
         textAlign: TextAlign.center,
         maxLength: 1,
         style: TextStyle(
-          fontSize: boxSize * 0.55,
+          fontSize: boxSize * 0.85,
           fontWeight: FontWeight.bold,
           color: Colors.black,
         ),
         decoration: InputDecoration(
           counterText: '',
           filled: true,
-          fillColor: Colors.transparent,
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(6),
-            borderSide: BorderSide.none,
-          ),
+          fillColor: Colors.white,
           contentPadding: EdgeInsets.zero,
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(4),
+            borderSide: const BorderSide(color: Colors.black, width: 2),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(4),
+            borderSide: const BorderSide(color: Colors.blue, width: 3),
+          ),
         ),
         textCapitalization: TextCapitalization.characters,
       ),
@@ -58,8 +63,8 @@ class _LessonOneDayOneActThreeState extends State<LessonOneDayOneActThree> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
-    final double boxSize = clampDouble(size.width * 0.065, 30, 48);
-    final double spacing = clampDouble(size.width * 0.012, 4, 8);
+    final double boxSize = clampDouble(size.width * 0.055, 24, 38);
+    final double spacing = clampDouble(size.width * 0.006, 2, 5);
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
@@ -73,16 +78,55 @@ class _LessonOneDayOneActThreeState extends State<LessonOneDayOneActThree> {
             ),
           ),
 
-          /// 14 INPUT BOXES
+          /// 14 INPUT BOXES HORIZONTAL
           Positioned(
-            left: size.width * 0.06,
-            right: size.width * 0.06,
+            left: size.width * 0.03,
+            right: size.width * 0.03,
             bottom: size.height * 0.085,
-            child: Wrap(
-              alignment: WrapAlignment.center,
-              spacing: spacing,
-              runSpacing: spacing,
-              children: List.generate(14, (index) => inputBox(index, boxSize)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(
+                14,
+                (index) => Padding(
+                  padding: EdgeInsets.symmetric(horizontal: spacing),
+                  child: inputBox(index, boxSize),
+                ),
+              ),
+            ),
+          ),
+
+          /// HOME BUTTON
+          Positioned(
+            top: clampDouble(size.height * 0.025, 14, 22),
+            right: clampDouble(size.width * 0.04, 12, 20),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (_) => const Lesson1Screen()),
+                );
+              },
+              child: Container(
+                width: clampDouble(size.width * 0.14, 50, 70),
+                height: clampDouble(size.width * 0.14, 50, 70),
+                decoration: BoxDecoration(
+                  color: Colors.orange,
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.white, width: 4),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 8,
+                      offset: Offset(0, 4),
+                    ),
+                  ],
+                ),
+                child: Icon(
+                  Icons.home,
+                  color: Colors.white,
+                  size: clampDouble(size.width * 0.08, 28, 40),
+                ),
+              ),
             ),
           ),
         ],
