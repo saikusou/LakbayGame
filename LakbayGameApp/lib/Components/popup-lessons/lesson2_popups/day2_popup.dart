ee03df8de2a9b75b24cb3aabc91187efcd33cc5f
viper357621 <87564641+viper357621@users.noreply.github.com>
2026-06-13 12:27:01 +0800

    ..

--- LakbayGameApp/lib/Components/popup-lessons/lesson2_popups/day2_popup.dart
+++ LakbayGameApp/lib/Components/popup-lessons/lesson2_popups/day2_popup.dart
@@ -5,6 +5,8 @@ import 'package:lakbay_game/Views/game/lesson-two/day-one/act3.dart';
 import 'package:lakbay_game/Views/game/lesson-two/day-one/act4.dart';
 import 'package:lakbay_game/Views/game/lesson-two/day-one/act5.dart';
 import 'package:lakbay_game/Views/game/lesson-two/day-two/act2.dart';
+import 'package:lakbay_game/Views/game/lesson-two/day-two/act3.dart';
+import 'package:lakbay_game/Views/game/lesson-two/day-two/act4.dart';
 import 'package:lakbay_game/models/user_model.dart';
 
 class Day2Popup extends StatelessWidget {
@@ -36,6 +38,10 @@ class Day2Popup extends StatelessWidget {
 
     if (title.contains('Picture mining')) {
       return _TamaOMaliPopup(user: user);
+    }
+
+    if (title.contains('Piliin ang tamang panahon')) {
+      return _TamangPanahonPopup(user: user);
     }
 
     /// 5. TAKDANG ARALIN
@@ -313,7 +319,7 @@ class _TamaOMaliPopup extends StatelessWidget {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => LessonTwoDayOneActThree(user: user),
+                      builder: (context) => LessonTwoDayTwoActThree(user: user),
                     ),
                   );
                 },
@@ -344,9 +350,106 @@ class _TamaOMaliPopup extends StatelessWidget {
   }
 }
 
-/// =========================================================
-/// 4. TAMA O MALI
-/// =========================================================
+class _TamangPanahonPopup extends StatelessWidget {
+  final UserModel user;
+
+  const _TamangPanahonPopup({required this.user});
+
+  double clampDouble(double value, double min, double max) {
+    return value.clamp(min, max).toDouble();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    final popupHeight = clampDouble(size.height * 0.75, 450, 530);
+    final popupWidth = clampDouble(size.width * 0.90, 350, 550);
+
+    return Dialog(
+      backgroundColor: Colors.transparent,
+      insetPadding: const EdgeInsets.all(18),
+      child: Container(
+        width: popupWidth,
+        height: popupHeight,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(28),
+          border: Border.all(color: Colors.blue, width: 5),
+
+          /// IMAGE FILLS ENTIRE POPUP
+          image: const DecorationImage(
+            image: AssetImage('assets/lesson-two-day2-act4t.png'),
+            fit: BoxFit.fill,
+          ),
+        ),
+
+        child: Column(
+          children: [
+            /// CLOSE BUTTON
+            Align(
+              alignment: Alignment.topRight,
+              child: Padding(
+                padding: const EdgeInsets.all(14),
+                child: GestureDetector(
+                  onTap: () => Navigator.pop(context),
+                  child: Container(
+                    width: 44,
+                    height: 44,
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.white, width: 3),
+                    ),
+                    child: const Icon(
+                      Icons.close,
+                      color: Colors.white,
+                      size: 26,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            const Spacer(),
+
+            /// SUBMIT BUTTON
+            Padding(
+              padding: const EdgeInsets.only(bottom: 5),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => LessonTwoDayTwoActFour(user: user),
+                    ),
+                  );
+                },
+
+                child: Container(
+                  width: 55,
+                  height: 55,
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.white, width: 4),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 8,
+                        offset: Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                  child: const Icon(Icons.send, color: Colors.white, size: 30),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
 class _SubukanPopup extends StatelessWidget {
   final UserModel user;
