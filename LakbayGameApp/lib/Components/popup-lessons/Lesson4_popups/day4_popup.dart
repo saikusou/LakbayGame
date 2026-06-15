cddd41621a2e37c199bceb943223aa3651e3da3b
viper357621 <87564641+viper357621@users.noreply.github.com>
2026-06-15 20:37:43 +0800

    ..

--- LakbayGameApp/lib/Components/popup-lessons/Lesson4_popups/day4_popup.dart
+++ LakbayGameApp/lib/Components/popup-lessons/Lesson4_popups/day4_popup.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:lakbay_game/Views/game/lesson-three/day-three/act3.dart';
-import 'package:lakbay_game/Views/game/lesson-two/day-three/act2.dart';
-import 'package:lakbay_game/Views/game/lesson-two/day-three/act3.dart';
-import 'package:lakbay_game/Views/game/lesson-two/day-three/act4.dart';
-import 'package:lakbay_game/Views/game/lesson-two/day-three/act5.dart';
+import 'package:lakbay_game/Views/game/lesson-four/day-four/act2.dart';
+import 'package:lakbay_game/Views/game/lesson-four/day-four/act3.dart';
 import 'package:lakbay_game/models/user_model.dart';
 
 class Day4Popup extends StatelessWidget {
@@ -19,43 +16,12 @@ class Day4Popup extends StatelessWidget {
       return _LearningObjectivesPopup(user: user);
     }
 
-    if (title.contains('Tama o Mali E-React Mo')) {
-      return _TamaOMaliPopup(user: user);
-    }
-
-    /// 3. KONSEPTO
-    if (title.contains('Ang Aking Pamilya sa Pamayanan')) {
+    if (title.contains('Pagsusulit')) {
       Future.microtask(() {
         Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (_) => LessonThreeDayOneActThree(user: user),
-          ),
-        );
-      });
-
-      return const SizedBox.shrink();
-    }
-
-    if (title.contains('Guhit mo Ibahagi mo')) {
-      Future.microtask(() {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (_) => LessonTwoDayThreeActThree(user: user),
-          ),
-        );
-      });
-
-      return const SizedBox.shrink();
-    }
-
-    if (title.contains('Pagninilay')) {
-      Future.microtask(() {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (_) => LessonTwoDayThreeActFour(user: user),
+            builder: (_) => LessonFourDayFourActTwo(user: user),
           ),
         );
       });
@@ -69,7 +35,7 @@ class Day4Popup extends StatelessWidget {
         Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (_) => LessonTwoDayThreeActFive(user: user),
+            builder: (_) => LessonFourDayFourActThree(user: user),
           ),
         );
       });
@@ -114,7 +80,7 @@ class _LearningObjectivesPopup extends StatelessWidget {
 
           /// BACKGROUND IMAGE
           image: const DecorationImage(
-            image: AssetImage('assets/lesson-two-day3-act1t.png'),
+            image: AssetImage('assets/lesson-four-day4-act1.jpg'),
             fit: BoxFit.fill,
           ),
         ),
@@ -154,271 +120,6 @@ class _LearningObjectivesPopup extends StatelessWidget {
 
             /// OPTIONAL SPACE
             const Spacer(),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class _TamaOMaliPopup extends StatelessWidget {
-  final UserModel user;
-
-  const _TamaOMaliPopup({required this.user});
-
-  double clampDouble(double value, double min, double max) {
-    return value.clamp(min, max).toDouble();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-
-    final popupHeight = clampDouble(size.height * 0.75, 450, 530);
-    final popupWidth = clampDouble(size.width * 0.90, 350, 550);
-
-    return Dialog(
-      backgroundColor: Colors.transparent,
-      insetPadding: const EdgeInsets.all(18),
-
-      child: Container(
-        width: popupWidth,
-        height: popupHeight,
-
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(28),
-
-          border: Border.all(color: Colors.blue, width: 5),
-
-          /// BACKGROUND IMAGE
-          image: const DecorationImage(
-            image: AssetImage('assets/lesson-two-day3-act2t.png'),
-            fit: BoxFit.fill,
-          ),
-        ),
-
-        child: Column(
-          children: [
-            /// CLOSE BUTTON
-            Align(
-              alignment: Alignment.topRight,
-
-              child: Padding(
-                padding: const EdgeInsets.all(14),
-
-                child: GestureDetector(
-                  onTap: () => Navigator.pop(context),
-
-                  child: Container(
-                    width: 44,
-                    height: 44,
-
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      shape: BoxShape.circle,
-
-                      border: Border.all(color: Colors.white, width: 3),
-                    ),
-
-                    child: const Icon(
-                      Icons.close,
-                      color: Colors.white,
-                      size: 26,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-
-            /// PUSH BUTTON TO BOTTOM
-            const Spacer(),
-
-            /// SUBMIT BUTTON
-            Padding(
-              padding: const EdgeInsets.only(bottom: 20),
-
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          LessonThreeDayThreeActTwo(user: user),
-                    ),
-                  );
-                },
-
-                child: Container(
-                  width: 65,
-                  height: 55,
-
-                  decoration: BoxDecoration(
-                    color: Colors.green,
-                    shape: BoxShape.circle,
-
-                    border: Border.all(color: Colors.white, width: 4),
-
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.black26,
-                        blurRadius: 8,
-                        offset: Offset(0, 4),
-                      ),
-                    ],
-                  ),
-
-                  child: const Icon(Icons.send, color: Colors.white, size: 30),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-/// =========================================================
-/// 5. TAKDANG ARALIN
-/// =========================================================
-
-class _TakdangAralinPopup extends StatelessWidget {
-  final UserModel user;
-  const _TakdangAralinPopup({required this.user});
-
-  double clampDouble(double value, double min, double max) {
-    return value.clamp(min, max).toDouble();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-
-    final popupHeight = clampDouble(size.height * 0.75, 450, 500);
-    final popupWidth = clampDouble(size.width * 0.90, 350, 550);
-
-    return Dialog(
-      backgroundColor: Colors.transparent,
-      insetPadding: const EdgeInsets.all(18),
-
-      child: Container(
-        width: popupWidth,
-        height: popupHeight,
-
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(28),
-
-          border: Border.all(color: Colors.blue, width: 5),
-
-          /// BACKGROUND IMAGE
-          image: const DecorationImage(
-            image: AssetImage('assets/lesson-three-day3-act5.png'),
-            fit: BoxFit.cover,
-          ),
-        ),
-
-        child: Column(
-          children: [
-            /// CLOSE BUTTON
-            Align(
-              alignment: Alignment.topRight,
-
-              child: Padding(
-                padding: const EdgeInsets.all(14),
-
-                child: GestureDetector(
-                  onTap: () => Navigator.pop(context),
-
-                  child: Container(
-                    width: 44,
-                    height: 44,
-
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      shape: BoxShape.circle,
-
-                      border: Border.all(color: Colors.white, width: 3),
-                    ),
-
-                    child: const Icon(
-                      Icons.close,
-                      color: Colors.white,
-                      size: 26,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-
-            /// OPTIONAL SPACE
-            const Spacer(),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-/// =========================================================
-/// REUSABLE POPUP CONTAINER
-/// =========================================================
-class _CustomPopupContainer extends StatelessWidget {
-  final Color borderColor;
-  final UserModel user;
-  final Widget child;
-
-  const _CustomPopupContainer({
-    required this.borderColor,
-    required this.user,
-    required this.child,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Dialog(
-      backgroundColor: Colors.transparent,
-      insetPadding: const EdgeInsets.all(18),
-
-      child: Container(
-        width: 360,
-        padding: const EdgeInsets.all(18),
-
-        decoration: BoxDecoration(
-          color: const Color(0xFFFFF6D8),
-          borderRadius: BorderRadius.circular(28),
-
-          border: Border.all(color: borderColor, width: 5),
-        ),
-
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            /// CLOSE BUTTON
-            Align(
-              alignment: Alignment.topRight,
-
-              child: GestureDetector(
-                onTap: () => Navigator.pop(context),
-
-                child: Container(
-                  width: 44,
-                  height: 44,
-
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    shape: BoxShape.circle,
-
-                    border: Border.all(color: Colors.white, width: 3),
-                  ),
-
-                  child: const Icon(Icons.close, color: Colors.white, size: 26),
-                ),
-              ),
-            ),
-
-            const SizedBox(height: 10),
-
-            child,
           ],
         ),
       ),
---
+++ LakbayGameApp/lib/Views/game/lesson-four/day-four/act2.dart
@@ -0,0 +1,213 @@
+import 'package:flutter/material.dart';
+import 'package:lakbay_game/Views/lesson1.dart';
+import 'package:lakbay_game/Views/lesson4.dart';
+import 'package:lakbay_game/models/user_model.dart';
+
+class LessonFourDayFourActTwo extends StatefulWidget {
+  final UserModel user;
+
+  const LessonFourDayFourActTwo({super.key, required this.user});
+
+  @override
+  State<LessonFourDayFourActTwo> createState() =>
+      _LessonFourDayFourActTwoState();
+}
+
+class _LessonFourDayFourActTwoState extends State<LessonFourDayFourActTwo> {
+  int currentImage = 0;
+
+  final List<String> images = [
+    'assets/lesson-four-day4-act5.jpg',
+    'assets/lesson-four-day4-act5a.jpg',
+    'assets/lesson-four-day4-act5b.jpg',
+    'assets/lesson-four-day4-act5c.jpg',
+  ];
+
+  void nextImage() {
+    if (currentImage < images.length - 1) {
+      setState(() {
+        currentImage++;
+      });
+    }
+  }
+
+  void previousImage() {
+    if (currentImage > 0) {
+      setState(() {
+        currentImage--;
+      });
+    }
+  }
+
+  double clampDouble(double value, double min, double max) {
+    return value.clamp(min, max).toDouble();
+  }
+
+  Widget circleButton({
+    required IconData icon,
+    required Color color,
+    required double size,
+    required VoidCallback onTap,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: size,
+        height: size,
+        decoration: BoxDecoration(
+          color: color,
+          shape: BoxShape.circle,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.25),
+              blurRadius: 6,
+              offset: const Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Icon(icon, color: Colors.white, size: size * 0.55),
+      ),
+    );
+  }
+
+  Widget navButton({
+    required String text,
+    required VoidCallback onPressed,
+    required double fontSize,
+    required double horizontalPadding,
+    required double verticalPadding,
+  }) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.orange,
+        foregroundColor: Colors.white,
+        padding: EdgeInsets.symmetric(
+          horizontal: horizontalPadding,
+          vertical: verticalPadding,
+        ),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      ),
+      child: Text(
+        text,
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: fontSize),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+
+    final double sidePadding = screenSize.width * 0.04;
+    final double iconTop = screenSize.height * 0.05;
+    final double homeSize = clampDouble(screenSize.width * 0.12, 42, 70);
+
+    final double buttonFont = clampDouble(screenSize.width * 0.035, 13, 18);
+    final double buttonHPadding = clampDouble(screenSize.width * 0.06, 18, 30);
+    final double buttonVPadding = clampDouble(
+      screenSize.height * 0.012,
+      10,
+      16,
+    );
+
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Image.asset(images[currentImage], fit: BoxFit.fill),
+            ),
+
+            Positioned(
+              top: iconTop,
+              right: sidePadding,
+              child: circleButton(
+                icon: Icons.home,
+                color: Colors.orange,
+                size: homeSize,
+                onTap: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => Lesson4Screen(user: widget.user),
+                    ),
+                  );
+                },
+              ),
+            ),
+
+            Positioned(
+              top: iconTop + homeSize + 8,
+              right: sidePadding,
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 6,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.black54,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Text(
+                  '${currentImage + 1}/${images.length}',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: buttonFont,
+                  ),
+                ),
+              ),
+            ),
+
+            Positioned(
+              left: 20,
+              right: 20,
+              bottom: 5,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  currentImage > 0
+                      ? navButton(
+                          text: 'Previous',
+                          onPressed: previousImage,
+                          fontSize: buttonFont,
+                          horizontalPadding: buttonHPadding,
+                          verticalPadding: buttonVPadding,
+                        )
+                      : SizedBox(
+                          width: clampDouble(screenSize.width * 0.28, 90, 130),
+                        ),
+
+                  currentImage < images.length - 1
+                      ? navButton(
+                          text: 'Next',
+                          onPressed: nextImage,
+                          fontSize: buttonFont,
+                          horizontalPadding: buttonHPadding,
+                          verticalPadding: buttonVPadding,
+                        )
+                      : navButton(
+                          text: 'Done',
+                          onPressed: () {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (_) =>
+                                    Lesson1Screen(user: widget.user),
+                              ),
+                            );
+                          },
+                          fontSize: buttonFont,
+                          horizontalPadding: buttonHPadding,
+                          verticalPadding: buttonVPadding,
+                        ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
