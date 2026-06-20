c78091ab71c4cca256dd6c113e7601e7ebe71a93
ksai <83051864+saikusou@users.noreply.github.com>
2026-06-20 13:38:23 +0800

    pushj

---
+++ LakbayGameApi/Controllers/PointsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LakbayGameApi.Controllers
+{
+    [Route("points")]
+    [ApiController]
+    public class PointsController : ControllerBase
+    {
+
+    }
+}
---
+++ LakbayGameApi/Models/Points.cs
@@ -0,0 +1,8 @@
+﻿namespace LakbayGameApi.Models
+{
+    public class Points
+    {
+        public int UserId { get; set; }
+        public required string CountedPoints { get; set; }
+    }
+}
